acb800368459765a878effcf0edee45d00bc3338
Davg1700 <davg1700@gmail.com>
2022-12-26 13:04:57 -0700

    bug fixes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+import './App.css';
+import { render } from 'react-dom';
+
+function App() {
+ render() {
+    return (
+        
+    )
+ }
+}
